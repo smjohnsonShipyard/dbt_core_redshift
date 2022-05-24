3e4aed4a87e3f0dae1af283d1ea7615a2db9fc2f
Steven <steven.johnson@shipyardapp.com>
2022-05-24 18:34:05 -0400

    Update execute_dbt.py

--- execute_dbt.py
+++ execute_dbt.py
@@ -3,5 +3,8 @@ import os
 import json
 
 dbt_command = os.environ.get('DBT_COMMAND', 'dbt run')
+directory_of_file = os.path.dirname(os.path.realpath(__file__))
 
+
+os.chdir(directory_of_file)
 subprocess.run(['sh', '-c', dbt_command], check=True)
